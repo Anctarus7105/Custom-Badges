01fe89867aa7c4278f49adb65aaf23ae88943c3c
Anctarus7105 <idzhabkin@gmail.com>
2025-12-23 17:40:09 +0400

    Create Deer God.lua

---
+++ Hardcore Mode/Deer God.lua
@@ -0,0 +1,10 @@
+---====== Load achievement giver ======---
+local achievementGiver = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Custom%20Achievements/Source.lua"))()
+
+---====== Display achievement ======---
+achievementGiver({
+    Title = "Out Of Many Looks",
+    Desc = "Do ot Look At It...",
+    Reason = "Survive The Rare Entity Called Dear God",
+    Image = "rbxassetid://11395249132"
+})
