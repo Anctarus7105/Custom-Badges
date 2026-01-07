c28c87a6e41abcd387501b30b70e11817a489ccc
Anctarus7105 <idzhabkin@gmail.com>
2026-01-07 23:46:10 +0400

    Create Hunger.lua

---
+++ Fear Mode/Hunger.lua
@@ -0,0 +1,10 @@
+---====== Load achievement giver ======---
+local achievementGiver = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Custom%20Achievements/Source.lua"))()
+
+---====== Display achievement ======---
+achievementGiver({
+    Title = "God-Tier Gamer",
+    Desc = "Congrats!!!",
+    Reason = "Survive the entity "NIGHTMARE_HUNGER"",
+    Image = "rbxassetid://0"
+})
